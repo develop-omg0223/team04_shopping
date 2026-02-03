2cec2d1381c02eaef069671bcea5ae69064d5da1
develop-omg0223 <lalalu0321@gmail.com>
2026-02-03 17:13:01 +0900

    기본 파일 추가

---
+++ team4_shopping/src/com/koreait/dao/DBConnector.java
@@ -0,0 +1,40 @@
+package com.koreait.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//db 연결
+public class DBConnector {
+
+	public static Connection getConnection() {
+
+		Connection connection = null;
+
+		// 연결에 필요한 정보
+		String userName = "kdt";
+		String password = "1234";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		// jdbc url 구글링하면 나옴
+		// url - 주소 / 어디로 전달할래?
+
+		try {
+			// 드라이버를 메모리에 할당
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// 문제가 없으면 getConnection 객체에 정보를 알려준다 (url,userName, passWord)
+			connection = DriverManager.getConnection(url, userName, password);
+			// SQLException 연결 정보가 잘 못 되었을 때 연결정보 예외
+			System.out.println("-연결 성공-");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("드라이버 로딩 실패");
+		} catch (SQLException e) {
+			System.out.println("연결정보 오류");
+			e.printStackTrace();
+		}
+
+		return connection;
+
+	}
+
+}
