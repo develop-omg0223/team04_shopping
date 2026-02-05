2336860bc68783dfb0ec681abdb873cfab49db33
mickyun1633 <mickyun1633@gmail.com>
2026-02-05 10:35:51 +0900

    26.02.05 10:35 윤철민 API 주석 추가

--- team4_shopping/src/com/koreait/dto/ItemDTO.java
+++ team4_shopping/src/com/koreait/dto/ItemDTO.java
@@ -1,19 +1,31 @@
 package com.koreait.dto;
 
+/**
+ * @author 윤철민
+ * 
+ * @since jdk17
+ * 
+ * Item 정보를 담기 위한 DTO 클래스
+ * 
+ * DB의 Item 테이블 구조를 그대로 반영하여
+ * 상품 번호, 상품명, 카테고리, 가격, 재고 정보를 저장
+ * 
+ * 데이터를 전달하는 용도로 사용
+ */
+
 
 /* 	ITEM_NUMBER NUMBER
-,ITEM_NAME VARCHAR2(100) NOT NULL
-,ITEM_CAT VARCHAR2(100) CHECK(ITEM_CAT IN('모자', '상의', '하의', '신발')) NOT NULL
-,ITEM_PRICE NUMBER DEFAULT 0 NOT NULL 
-,ITEM_STOCK NUMBER DEFAULT 0 NOT NULL */
+	,ITEM_NAME VARCHAR2(100) NOT NULL
+	,ITEM_CAT VARCHAR2(100) CHECK(ITEM_CAT IN('모자', '상의', '하의', '신발')) NOT NULL
+	,ITEM_PRICE NUMBER DEFAULT 0 NOT NULL 
+	,ITEM_STOCK NUMBER DEFAULT 0 NOT NULL 	*/
 
 public class ItemDTO {
-//	 상품 DTO : 철민	
-	private int itemNumber;
-	private String itemName;
-	private String itemCat;
-	private int itemPrice;
-	private int itemStock;
+		private int itemNumber;
+		private String itemName;
+		private String itemCat;
+		private int itemPrice;
+		private int itemStock;
 	
 	
 	public ItemDTO() {
